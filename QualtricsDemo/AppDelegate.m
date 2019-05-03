3430394297b82622fd4dcd5963da09d6dd317051
markradacz <Johnny316>
2019-05-03 00:41:39 -0400

    updated to valid test values

--- QualtricsDemo/AppDelegate.m
+++ QualtricsDemo/AppDelegate.m
@@ -40,17 +40,14 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    [Qualtrics.shared initializeWithBrandId:@"geha" zoneId:@"" interceptId:@"" completion:^(InitializationResult * _Nonnull result) {
-        
+    [Qualtrics.shared initializeWithBrandId:@"qcorp" zoneId:@"ZN_cBIa8S111A9bAYR" interceptId:@"SI_6G7KeA6Iom2tdcN" completion:^(InitializationResult * _Nonnull result) {
+
         NSLog(@"Sucees result %@",result);
     }];
-
 }
-
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
-
 
 @end
